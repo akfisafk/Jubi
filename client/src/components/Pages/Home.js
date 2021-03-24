9352f95cab973682e6d6a3b962e2f0b510a4717e
Brandon <brandinophan@gmail.com>
2021-03-23 22:08:29 -0700

    added login loading spinner

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -10,12 +10,13 @@ const initialState = { firstName: '', lastName: '', email: '', password: '' };
 export const Home = () => {
     const isSignup = false;
     const [formData, setFormData] = useState(initialState);
+    const [loading, setLoading] = useState(true);
     const dispatch = useDispatch();
     const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
+        handleLoading();
         if (isSignup) {
             console.log(isSignup);
             dispatch(signup(formData, history));
@@ -26,6 +27,16 @@ export const Home = () => {
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value})
+    }
+
+    const handleLoading = () => {
+        setLoading(false);
+        
+        const trueLoading = () => {
+            setLoading(true);
+        }
+
+        setTimeout(trueLoading, 5000);
     }
 
     useEffect(() => {
@@ -47,7 +58,9 @@ export const Home = () => {
                         <form className="landing__login__form" autoComplete="off" onSubmit={handleSubmit}>
                             <FormInput name="email" label="Email" type="email" handleChange={handleChange}/>
                             <FormInput name="password" label="Password" type="password" handleChange={handleChange} />
-                            <button type="submit"><span>{isSignup ? 'Sign Up' : 'Login'}</span></button>
+                            <button type="submit"><span>{loading ? 'Login' : <i
+                                className="fa fa-refresh fa-spin"
+                            />}</span></button>
                         </form>        
                     </div>
                 </div>
